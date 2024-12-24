bad1b720077a89e40f9ae9e6a54193939ecdef0c
Devavarshinik <devavarshinik.24cse@kongu.edu>
2024-12-24 11:30:35 +0530

    First project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$x = 10; 
+$y = 4; 
+
+// Performs Arithmetic operations 
+// and display the result 
+echo($x + $y) . "</br>"; 
+echo($x - $y) . "</br>"; 
+echo($x * $y) . "</br>"; 
+echo($x / $y) . "</br>"; 
+echo($x % $y); 
+
+?>
